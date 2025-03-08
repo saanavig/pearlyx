9e3abbcb2cf3c500b87b7af0ec478c01ffdbbd61
srewashimondal <mondalsrewashi2004@gmail.com>
2025-03-08 12:08:22 -0500

    updated routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,19 @@
-import './index.css'
+import './index.css';  // Ensure this is the first import
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import App from './App.tsx';
+import About from './About.tsx';
+import Analyze from './Analyze.tsx';
 
-import App from './App.tsx'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/analyze" element={<Analyze />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
